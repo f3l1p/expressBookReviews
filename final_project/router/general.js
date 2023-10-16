6327d2a6c3a7fa21fd524bfea277412cf7fcf9a2
f3l1p <wnrad.f@gmail.com>
2023-10-16 20:48:20 +0200

    add promises to step 1 2 3 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 let books = require("./booksdb.js");
 const axios = require("axios");
 let isValid = require("./auth_users.js").isValid;
@@ -40,41 +40,68 @@ public_users.post("/register", (req, res) => {
   return res.status(404).json({ message: "Unable to register user." });
 });
 
-
-
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async (req, res) => {
   let isbNum = req.params.isbn;
+  try {
+    const response = await axios.get("http://localhost:5000/books");
+    const jsonData = response.data;
 
-  return res.status(300).json(books[isbNum]);
+    res.status(300).json(jsonData[isbNum]);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the JSON file." });
+  }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async (req, res) => {
   let author = req.params.author;
 
-  const filteredBooks = {};
-  Object.keys(books).forEach((key) => {
-    if (books[key].author === author) {
-      filteredBooks[key] = books[key];
-    }
-  });
+  try {
+    const response = await axios.get("http://localhost:5000/books");
+    const jsonData = response.data;
 
-  return res.status(300).json(filteredBooks);
+    const filteredBooks = {};
+    Object.keys(books).forEach((key) => {
+      if (books[key].author === author) {
+        filteredBooks[key] = books[key];
+      }
+    });
+
+    res.status(300).json(filteredBooks);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the JSON file." });
+  }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async (req, res) => {
   let title = req.params.title;
 
-  const filteredBooks = {};
-  Object.keys(books).forEach((key) => {
-    if (books[key].title === title) {
-      filteredBooks[key] = books[key];
-    }
-  });
+  try {
+    const response = await axios.get("http://localhost:5000/books");
+    const jsonData = response.data;
 
-  return res.status(300).json(filteredBooks);
+    const filteredBooks = {};
+    Object.keys(books).forEach((key) => {
+      if (books[key].title === title) {
+        filteredBooks[key] = books[key];
+      }
+    });
+
+    res.status(300).json(filteredBooks);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the JSON file." });
+  }
 });
 
 //  Get book review
